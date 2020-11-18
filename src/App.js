c896f17597552d16d6b012e6b5b701e06e9f6458
Amit Biderman <amitbiderman1@gmail.com>
2020-11-18 22:13:16 +0200

    edited travis file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Amit The King
+          Amit king
         </p>
         <a
           className="App-link"
